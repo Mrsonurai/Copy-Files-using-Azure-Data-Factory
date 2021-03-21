a834f1ceb0135bb6ba0c149ca34778ffff7b5ff9
Sonu Rai <sonuraimr@gmail.com>
2021-03-21 14:47:17 +0530

    Create 02-create-metadata-table.sql

---
+++ 02-create-metadata-table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE ExportedTables (
+	ID int identity(1,1) Primary Key,
+	Table_Schema varchar(100) not null,
+	Table_Name varchar(100) not null
+);
+GO
+INSERT INTO ExportedTables(Table_Schema, Table_Name)
+VALUES
+('dbo','Cars'),
+('dbo','Movies')
+© 2021 GitHub, Inc.
